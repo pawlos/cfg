adb14cac0cdd62759c5d40d4b2f5f3ad19b6a379
Paweł Łukasik <lukasik.pawel+github@gmail.com>
2022-02-18 22:48:16 +0100

    Refactor: Extracted interface IFormatter
    Interface and first implementation for dotFormatter

---
+++ cfg/cfg/Formatters/IFormatter.cs
@@ -0,0 +1,13 @@
+﻿using Mono.Cecil.Cil;
+using System.Collections.Generic;
+
+namespace cfg.Formatters;
+
+internal interface IFormatter
+{
+    void PrintHeader();
+    void PrintFooter();
+    void PrintBB(Instruction leader, IEnumerable<Instruction> opCodes);
+    void PrintEdge(Instruction leader, Instruction nextLeader);
+}
+
---
+++ cfg/cfg/Formatters/dotFormatter.cs
@@ -0,0 +1,39 @@
+﻿using Mono.Cecil.Cil;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace cfg.Formatters;
+
+internal class dotFormatter : IFormatter
+{
+    public dotFormatter(Action<string> printer)
+    {
+        Printer = printer;
+    }
+
+    public void PrintHeader()
+    {
+        Printer("digraph CFG {");
+        Printer("node [shape=box]");
+    }
+
+    public void PrintFooter()
+    {
+        Printer("}");
+    }
+
+    public void PrintBB(Instruction leader, IEnumerable<Instruction> opCodes)
+    {
+        Printer($@"IL_{leader.Offset:X4} [label=""{opCodes.Select(x => x.ToString().Replace(@"""", @"\""")).Aggregate((x, y) => x + "\\l" + y)}""]");
+    }
+
+    public void PrintEdge(Instruction leader, Instruction nextLeader)
+    {
+        var headPort = "";
+        Printer($"IL_{leader.Offset:X4} -> IL_{nextLeader.Offset:X4}{headPort}");
+    }
+
+    private Action<string> Printer { get; }
+}
+
